478287dd1f2dc258848d2a9f3f9952cc1d95f9ba
Bill Hefty <billhefty@gmail.com>
2016-12-03 08:38:04 -0600

    Complete handleAddTodo

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+let uuid = require('node-uuid')
 
 import TodoList from 'TodoList'
 import AddTodo from 'AddTodo'
@@ -12,26 +13,36 @@ class TodoApp extends Component {
       searchText: '',
       todos: [
         {
-          id: 1,
+          id: uuid(),
           text: 'Walk the dog'
         },
         {
-          id: 2,
+          id: uuid(),
           text: 'Clean the yard'
         },
         {
-          id: 3,
+          id: uuid(),
           text: 'Finish this lesson'
         },
         {
-          id: 4,
+          id: uuid(),
           text: 'Start next video'
         }
       ]
     }
+    this.handleAddTodo = this.handleAddTodo.bind(this)
+    this.handleSearch = this.handleSearch.bind(this)
   }
   handleAddTodo(text) {
-    alert('new todo: ' + text)
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: uuid(),
+          text: text
+        }
+      ]
+    })
   }
   handleSearch(showCompleted, searchText) {
     this.setState({
