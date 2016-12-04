367aea2a3e0336fba2d83f930262ec6c709c8bbf
Bill Hefty <billhefty@gmail.com>
2016-12-04 11:19:20 -0600

    Update todos on firebase

--- app/tests/components/Todo.test.jsx
+++ app/tests/components/Todo.test.jsx
@@ -4,6 +4,7 @@ import expect from 'expect'
 import $ from 'jQuery'
 import TestUtils from 'react-addons-test-utils'
 
+import * as actions from 'actions'
 import { Todo } from 'Todo'
 
 describe('Todo', () => {
@@ -17,15 +18,13 @@ describe('Todo', () => {
       text: 'Write todo.test.jsx test',
       completed: true
     }
+    let action = actions.startToggleTodo(todoData.id, !todoData.completed)
     let spy = expect.createSpy()
     let todo = TestUtils.renderIntoDocument(<Todo {...todoData} dispatch={spy}/>)
 
     let $el = $(ReactDOM.findDOMNode(todo))
     TestUtils.Simulate.click($el[0])
 
-    expect(spy).toHaveBeenCalledWith({
-      type: 'TOGGLE_TODO',
-      id: todoData.id
-    })
+    expect(spy).toHaveBeenCalledWith(action)
   })
 })
